9e83f89c413d71d50fb52f0343eb653090a79690
罗林 <exercisebank@163.com>
2024-12-26 23:00:01 +0800

    移除打包代码

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,27 +4,27 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('../views/home/index.vue')
+    component: () => import('@/views/home/index.vue')
   },
   {
     path: '/profile',
     name: 'Profile',
-    component: () => import('../views/profile/index.vue')
+    component: () => import('@/views/profile/index.vue')
   },
   {
     path: '/password-detail/:id',
     name: 'PasswordDetail',
-    component: () => import('../views/home/detail.vue')
+    component: () => import('@/views/home/detail.vue')
   },
   {
     path: '/add-password',
     name: 'AddPassword',
-    component: () => import('../views/home/add.vue')
+    component: () => import('@/views/home/add.vue')
   },
   {
     path: '/profile/edit',
     name: 'ProfileEdit',
-    component: () => import('../views/profile/edit.vue')
+    component: () => import('@/views/profile/edit.vue')
   }
 ]
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/password-assistant/',
+  base: './',
   plugins: [vue()],
   resolve: {
     alias: {
